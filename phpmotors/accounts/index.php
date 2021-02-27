065783e243754a0ca95413c4d46de7c052e40cb1
Gene Higgins <geneh@bulwarkpest.com>
2021-02-26 20:35:25 -0700

    fixed message issue

--- phpmotors/accounts/index.php
+++ phpmotors/accounts/index.php
@@ -60,7 +60,7 @@ switch ($action) {
     if ($regOutcome === 1) {
       setcookie('firstName',$clientFirstName, strtotime('+1 year'),'/');
       $_SESSION['message'] = "<p class='successMessage'>Thanks for registering $clientFirstName. Please use your email and password to login.</p>";   
-      header('Location: /phpmotors/accounts/?action=login');
+      header('Location: /phpmotors/accounts/?action=login-page');
     } else {
       $_SESSION['message'] = "<p class='errorMessage'>Sorry $clientFirstName, but the registration failed. Please try again.</p>";
     }
