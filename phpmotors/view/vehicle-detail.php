a47c6976aad04ab9eaca690c09a0f893d85375ba
hig19011 <hig19011@byui.edu>
2021-03-19 00:49:35 -0700

    Updated Layout and CSS

--- phpmotors/view/vehicle-detail.php
+++ phpmotors/view/vehicle-detail.php
@@ -1,8 +1,10 @@
 <?php
   displayGlobalMessage();
 
+  echo "<section class='details-container'>";
   echo $vehicleDisplay;
+  echo "<h3>More Images</h3>";
   echo $thumbnailsDisplay;
-  
+  echo "</section>";
 ?>
 
